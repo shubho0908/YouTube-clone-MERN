e1bd3e3aa6cf14a732fb82bf024107de9318261c
Shubhojeet Bera <shubhobera98@gmail.com>
2023-06-21 17:17:29 +0530

    working on video section

--- frontend/src/Components/VideoSection.jsx
+++ frontend/src/Components/VideoSection.jsx
@@ -5,6 +5,14 @@ import Navbar from "./Navbar";
 import "../Css/videoSection.css";
 import ReactLoading from "react-loading";
 import "plyr/dist/plyr.css";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import Tooltip from "@mui/material/Tooltip";
+import Zoom from "@mui/material/Zoom";
+import ThumbUpAltOutlinedIcon from "@mui/icons-material/ThumbUpAltOutlined";
+import ThumbDownOutlinedIcon from "@mui/icons-material/ThumbDownOutlined";
+import ReplyIcon from "@mui/icons-material/Reply";
+import { TfiDownload } from "react-icons/Tfi";
+import BookmarkAddOutlinedIcon from "@mui/icons-material/BookmarkAddOutlined";
 
 function VideoSection() {
   const { id } = useParams();
@@ -29,7 +37,10 @@ function VideoSection() {
   useEffect(() => {
     const initializePlyr = () => {
       if (!plyrInitialized && videoRef.current) {
-        const player = new Plyr(videoRef.current, { background: "red" });
+        const player = new Plyr(videoRef.current, {
+          background: "red",
+          ratio: null,
+        });
         setPlyrInitialized(true);
       }
     };
@@ -42,7 +53,7 @@ function VideoSection() {
   if (!videoData) {
     return (
       <>
-        <div className="main-video-section">
+        <div className="main-video-section2">
           <div className="spin2" style={{ height: "100vh" }}>
             <ReactLoading
               type={"spin"}
@@ -63,7 +74,8 @@ function VideoSection() {
     return <div>Video not found</div>;
   }
 
-  const { videoURL, Title, thumbnailURL } = matchedVideo;
+  const { videoURL, Title, thumbnailURL, ChannelProfile, uploader } =
+    matchedVideo;
 
   return (
     <>
@@ -79,8 +91,72 @@ function VideoSection() {
             >
               <source src={videoURL} type="video/mp4" />
             </video>
-            <h4>{Title}</h4>
           </div>
+          <p className="vid-title">{Title}</p>
+          <div className="some-channel-data">
+            <div className="channel-left-data">
+              <img src={ChannelProfile} alt="channelDP" className="channelDP" />
+              <div className="channel-data2">
+                <div className="creator">
+                  <p style={{ fontSize: "17px" }}>{uploader}</p>
+                  <Tooltip
+                    TransitionComponent={Zoom}
+                    title="Verified"
+                    placement="right"
+                  >
+                    <CheckCircleIcon
+                      fontSize="100px"
+                      style={{
+                        color: "rgb(138, 138, 138)",
+                        marginLeft: "4px",
+                      }}
+                    />
+                  </Tooltip>
+                </div>
+                <p className="channel-subs">10M subscribers</p>
+              </div>
+              <button className="subscribe">Subscribe</button>
+            </div>
+            <div className="channel-right-data">
+              <div className="like-dislike">
+                <div className="like-data">
+                  <ThumbUpAltOutlinedIcon
+                    fontSize="medium"
+                    style={{ color: "white" }}
+                  />
+                  <p className="like-count">250K</p>
+                </div>
+                <div className="vl"></div>
+                <div className="dislike-data">
+                  <ThumbDownOutlinedIcon
+                    fontSize="medium"
+                    style={{ color: "white" }}
+                  />
+                </div>
+              </div>
+              <div className="share">
+                <ReplyIcon
+                  fontSize="medium"
+                  style={{ color: "white", transform: "rotateY(180deg)" }}
+                />
+                <p className="share-txt">Share</p>
+              </div>
+              <div className="download-btn">
+                <h3>
+                  <TfiDownload />
+                </h3>
+                <p>Download</p>
+              </div>
+              <div className="save-later">
+                <BookmarkAddOutlinedIcon
+                  fontSize="medium"
+                  style={{ color: "white" }}
+                />
+                <p>Save</p>
+              </div>
+            </div>
+          </div>
+          <div className="description-section2"></div>
         </div>
         <div className="recommended-section"></div>
       </div>
