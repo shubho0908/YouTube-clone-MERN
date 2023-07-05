50797529c60d59a06dbf4cda02e4111321f07a32
Shubhojeet Bera <shubhobera98@gmail.com>
2023-07-05 15:53:48 +0530

    working on like video section

--- frontend/src/Components/LikeVideos.jsx
+++ frontend/src/Components/LikeVideos.jsx
@@ -7,18 +7,22 @@ import "../Css/likevideos.css";
 
 function LikeVideos() {
     const [email, setEmail] = useState();
+    const [name, setName] = useState();
     const [menuClicked, setMenuClicked] = useState(false);
     const [videolike, setLikedVideos] = useState([]);
 
     useEffect(() => {
         const token = localStorage.getItem("userToken");
         setEmail(jwtDecode(token).email);
+        setName(jwtDecode(token).name);
     }, []);
 
     useEffect(() => {
         const getLikeVideos = async () => {
             try {
-                const response = await fetch(`http://localhost:3000/getlikevideos/${email}`);
+                const response = await fetch(
+                    `http://localhost:3000/getlikevideos/${email}`
+                );
                 const result = await response.json();
                 setLikedVideos(result);
             } catch (error) {
@@ -48,38 +52,58 @@ function LikeVideos() {
         <>
             <Navbar />
             <LeftPanel />
-            <div className="like-video-sections" style={
-                menuClicked ? { left: "80px", width: "100%" } : { left: "255px" }
-            }>
-                <div className="like-left-section">
-                    <div className="page-cover">
-                        {videolike.length > 0 ? (
-                            <div className="firstvideo-thumbnail">
-                                <img src={videolike[0].thumbnailURL} alt="first-like-thumbnail" />
+            <div className="liked-video-data">
+                {videolike.length > 0 ? (
+                    <div
+                        className="like-video-sections"
+                        style={
+                            menuClicked ? { left: "80px", width: "100%" } : { left: "255px" }
+                        }
+                    >
+                        <div
+                            className="like-left-section"
+                            style={{
+                                backgroundImage: `url(${videolike[0]?.thumbnailURL})`
+                            }}
+                        >
+                            <div className="page-cover">
+                                {videolike && (
+                                    <div className="firstvideo-thumbnail">
+                                        <img
+                                            src={videolike[0].thumbnailURL}
+                                            alt="first-like-thumbnail"
+                                        />
+                                    </div>
+                                )}
+                                <div className="last-like-section">
+                                    <p className="like-head">Liked videos</p>
+                                    <p className="like-username">{name}</p>
+                                </div>
                             </div>
-                        ) : (<div className="spinner" style={{ height: "100vh" }}>
-                            <ReactLoading
-                                type={"spin"}
-                                color={"white"}
-                                height={50}
-                                width={50}
-                            />
-                        </div>)}
-                        <div className="last-like-section"></div>
+                        </div>
+                        <div className="like-right-section">
+                            {videolike.length > 0 ? videolike.map((element, index) => {
+                                return (
+                                    <div className="firstvideo-thumbnail" key={index}>
+                                        <img
+                                            src={element.thumbnailURL}
+                                            alt="first-like-thumbnail"
+                                        />
+                                    </div>
+                                )
+                            }) : ""}
+                        </div>
                     </div>
-                </div>
-                <div className="like-right-section">
-                    {videolike.length > 0 ? (
-                        <img src={videolike[0].thumbnailURL} alt="video-thumbnail" />
-                    ) : (<div className="spinner" style={{ height: "100vh" }}>
+                ) : (
+                    <div className="spinner" style={{ height: "100vh" }}>
                         <ReactLoading
                             type={"spin"}
                             color={"white"}
                             height={50}
                             width={50}
                         />
-                    </div>)}
-                </div>
+                    </div>
+                )}
             </div>
         </>
     );
