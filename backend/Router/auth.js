01aad7b7d1b05814126aca4afc29a518a5a9f2f5
Shubhojeet Bera <shubhobera98@gmail.com>
2023-09-09 13:38:10 +0530

    updated

--- backend/Router/auth.js
+++ backend/Router/auth.js
@@ -6,7 +6,7 @@ const jwt = require("jsonwebtoken");
 const userData = require("../Models/user");
 const auth = express.Router();
 const nodemailer = require("nodemailer");
-const URL = "https://shubho-youtube-mern.netlify.app";
+const URL = "https://youtube-clone-mern-backend.vercel.app";
 
 auth.post("/signup", async (req, res) => {
   try {
